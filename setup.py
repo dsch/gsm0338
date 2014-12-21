39a26e5a52d8429b9058d236a6c751dbdb662bf1
David Schneider <david.schneider@postmail.ch>
2014-12-21 10:46:23 +0100

    prepare package for upload to PyPi

--- setup.py
+++ setup.py
@@ -1,8 +1,39 @@
-from setuptools import setup
+from setuptools import setup, find_packages
+from codecs import open
+from os import path
 
-setup(name='gsm0338',
-      version='1.0',
-      description='GSM 03.38 codec',
-      packages=['gsm0338'],
-      install_requires=['six'],
-      )
+here = path.abspath(path.dirname(__file__))
+
+# Get the long description from the relevant file
+with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
+    long_description = f.read()
+
+setup(
+    name='gsm0338',
+    version='1.0a1',
+    description='GSM 03.38 codec',
+    long_description=long_description,
+
+    packages=find_packages(),
+    install_requires=['six'],
+    package_data={
+        '': ['*.txt', '*.rst'],
+        },
+
+    # metadata for upload to PyPI
+    author='David Schneider',
+    author_email='david.schneider@postmail.ch',
+    license='MIT',
+    url='https://github.com/dsch/gsm0338',
+    classifiers=[
+        'Development Status :: 3 - Alpha',
+        'Intended Audience :: Developers',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.6',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.3',
+        'Programming Language :: Python :: 3.4',
+        'Topic :: Software Development :: Libraries',
+    ],
+    )
