d2b601dfba97b7dc37de560aefd5ed5b632b66c5
David Schneider <david.schneider@postmail.ch>
2014-12-26 17:34:10 +0100

    run py.test as setup command "test"

--- setup.py
+++ setup.py
@@ -1,12 +1,31 @@
 from setuptools import setup, find_packages
+from setuptools.command.test import test as TestCommand
 from codecs import open
 from os import path
+import sys
 
 here = path.abspath(path.dirname(__file__))
 
 # Get the long description from the relevant file
 with open(path.join(here, 'README.rst'), encoding='utf-8') as f:
     long_description = f.read()
+
+
+class PyTest(TestCommand):
+
+    def finalize_options(self):
+        TestCommand.finalize_options(self)
+        self.test_args = [
+            'test',
+            '--pep8',
+        ]
+        self.test_suite = True
+
+    def run_tests(self):
+        #import here, cause outside the eggs aren't loaded
+        import pytest
+        errno = pytest.main(self.test_args)
+        sys.exit(errno)
 
 setup(
     name='gsm0338',
@@ -19,6 +38,12 @@ setup(
     package_data={
         '': ['*.txt', '*.rst'],
         },
+
+    tests_require=[
+        'pytest',
+        'pytest-pep8',
+    ],
+    cmdclass={'test': PyTest},
 
     # metadata for upload to PyPI
     author='David Schneider',
