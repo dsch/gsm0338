dcfed47d5c9422d1a7772ae0ee269bccfbf8a050
David Schneider <david.schneider@postmail.ch>
2015-01-02 15:27:08 +0100

    fix pep8 rule violation

--- test/test_codec.py
+++ test/test_codec.py
@@ -62,6 +62,8 @@ def test_encode_spanish_single_shift():
                                b'\x1B\x41\x1B\x49\x1B\x4F\x1B\x55\x1B\x61' \
                                b'\x1B\x65\x1B\x69\x1B\x6F\x1B\x75'
 
-    codec = gsm0338.Codec(single_shift_decode_map=gsm0338.SINGLE_SHIFT_CHARACTER_SET_SPANISH)
+    codec = gsm0338.Codec(
+        single_shift_decode_map=gsm0338.SINGLE_SHIFT_CHARACTER_SET_SPANISH
+    )
     assert codec.encode(unicode_spanish_single_shift) == \
         (gsm_spanish_single_shift, len(unicode_spanish_single_shift))
