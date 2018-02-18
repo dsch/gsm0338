091617bf6710d3e4993270937551ecd2b29d89b9
David Schneider <schneidav81@gmail.com>
2018-02-18 11:27:14 +0100

    cleanup module public interface

--- gsm0338/__init__.py
+++ gsm0338/__init__.py
@@ -3,6 +3,6 @@ import codecs
 from .charset import SINGLE_SHIFT_CHARACTER_SET_SPANISH
 from .codec import Codec, find_gsm0338, get_codec_info
 
-__all__ = ['SINGLE_SHIFT_CHARACTER_SET_SPANISH', 'Codec', 'find_gsm0338', 'get_codec_info']
+__all__ = ['SINGLE_SHIFT_CHARACTER_SET_SPANISH', 'Codec', 'get_codec_info']
 
 codecs.register(find_gsm0338)
